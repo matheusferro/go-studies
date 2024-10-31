45f74b55620e2b478c953c0b49a8773844ee9ff8
matheusferro <matheussouzaferro@gmail.com>
2024-10-30 22:11:38 -0300

    ident

--- library-backend/main.go
+++ library-backend/main.go
@@ -16,94 +16,94 @@ import (
 )
 
 type Album struct {
-    ID      string `json:"id" binding:"required"`
-    Title   string `json:"title" binding:"required"`
-    Artist  string `json:"artist" binding:"required"`
-    Price   float64 `json:"price" binding:"required"`
+	ID     string  `json:"id" binding:"required"`
+	Title  string  `json:"title" binding:"required"`
+	Artist string  `json:"artist" binding:"required"`
+	Price  float64 `json:"price" binding:"required"`
 }
 
 var (
-    dbClient pb.DatabaseClient
-    mutex sync.Mutex
+	dbClient pb.DatabaseClient
+	mutex    sync.Mutex
 )
 
 var albums = []Album{
-    {ID: "1", Title: "The Dark Side Of the Moon", Artist: "Pink Floyd", Price: 99.99},
-    {ID: "2", Title: "Black Sabbath", Artist: "Black Sabbath", Price: 99.99},
+	{ID: "1", Title: "The Dark Side Of the Moon", Artist: "Pink Floyd", Price: 99.99},
+	{ID: "2", Title: "Black Sabbath", Artist: "Black Sabbath", Price: 99.99},
 }
 
 func databaseConnection() pb.DatabaseClient {
-    mutex.Lock()
-    defer mutex.Unlock()
-    if dbClient == nil {
-        addr := fmt.Sprintf("%s:%s", os.Getenv("DB_ACL_HOST"), os.Getenv("DB_ACL_PORT"))
-	    conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	    if err != nil {
-	    	log.Fatalf("did not connect: %v", err)
-            return nil
-	    }
-        dbClient = pb.NewDatabaseClient(conn)
-    }
-    return dbClient
+	mutex.Lock()
+	defer mutex.Unlock()
+	if dbClient == nil {
+		addr := fmt.Sprintf("%s:%s", os.Getenv("DB_ACL_HOST"), os.Getenv("DB_ACL_PORT"))
+		conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			log.Fatalf("did not connect: %v", err)
+			return nil
+		}
+		dbClient = pb.NewDatabaseClient(conn)
+	}
+	return dbClient
 }
 
 func getAlbums(c *gin.Context) {
-    databaseConn := databaseConnection()
+	databaseConn := databaseConnection()
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	r, err := databaseConn.GetAllAlbums(ctx, &pb.Empty{})
 	if err != nil {
 		log.Fatalf("could not get albums: %v", err)
 	}
-    allAlbums := make([]*Album, 0, len(r.Albums))
-    for _, ab := range r.Albums {
-        //log.Printf("Album title: %s", ab.Title)
-        allAlbums = append(allAlbums, &Album{ID: ab.Id, Title: ab.Title, Artist: ab.Artist, Price: ab.Price})
-    }
-    c.IndentedJSON(http.StatusOK, allAlbums)
+	allAlbums := make([]*Album, 0, len(r.Albums))
+	for _, ab := range r.Albums {
+		//log.Printf("Album title: %s", ab.Title)
+		allAlbums = append(allAlbums, &Album{ID: ab.Id, Title: ab.Title, Artist: ab.Artist, Price: ab.Price})
+	}
+	c.IndentedJSON(http.StatusOK, allAlbums)
 
 }
 
 func getAlbumByID(c *gin.Context) {
-    id := c.Param("id")
+	id := c.Param("id")
 
-    for _, album := range albums {
-        if album.ID == id {
-            c.IndentedJSON(http.StatusOK, album)
-            return
-        }
-    }
+	for _, album := range albums {
+		if album.ID == id {
+			c.IndentedJSON(http.StatusOK, album)
+			return
+		}
+	}
 
-    c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
 
 func postAlbums(c *gin.Context) {
-    var newAlbum Album
-    if err := c.BindJSON(&newAlbum); err != nil {
-        c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid fields received"})
-        return
-    }
-    for _, album := range albums {
-        if album.ID == newAlbum.ID {
-            c.JSON(http.StatusConflict, gin.H{"message": "Resource already created"})
-            return
-        }
-    }
+	var newAlbum Album
+	if err := c.BindJSON(&newAlbum); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid fields received"})
+		return
+	}
+	for _, album := range albums {
+		if album.ID == newAlbum.ID {
+			c.JSON(http.StatusConflict, gin.H{"message": "Resource already created"})
+			return
+		}
+	}
 
-    albums = append(albums, newAlbum)
-    c.IndentedJSON(http.StatusCreated, newAlbum)
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
 func setupRouter() *gin.Engine {
-    router := gin.Default()
-    router.GET("/albums", getAlbums)
-    router.GET("/albums/:id", getAlbumByID)
-    router.POST("/albums", postAlbums)
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
+	router.POST("/albums", postAlbums)
 
-    return router
+	return router
 }
 
 func main() {
-    router := setupRouter()
-    router.Run("localhost:8080")
+	router := setupRouter()
+	router.Run("localhost:8080")
 }
--- library-backend/main_test.go
+++ library-backend/main_test.go
@@ -10,90 +10,84 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-
-
 func TestGetAllAlbums(t *testing.T) {
-    router := setupRouter()
+	router := setupRouter()
 
-    w := httptest.NewRecorder()
-    req, _ := http.NewRequest("GET", "/albums", nil)
-    router.ServeHTTP(w, req)
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/albums", nil)
+	router.ServeHTTP(w, req)
 
-    expectedResponse := `[{"id":"1","title":"The Dark Side Of the Moon","artist":"Pink Floyd","price":99.99},{"id":"2","title":"Black Sabbath","artist":"Black Sabbath","price":99.99}]`
-    assert.Equal(t, 200, w.Code)
-    assert.JSONEq(t, expectedResponse, w.Body.String())
+	expectedResponse := `[{"id":"1","title":"The Dark Side Of the Moon","artist":"Pink Floyd","price":99.99},{"id":"2","title":"Black Sabbath","artist":"Black Sabbath","price":99.99}]`
+	assert.Equal(t, 200, w.Code)
+	assert.JSONEq(t, expectedResponse, w.Body.String())
 }
 
 func TestGetAlbumByIDSuccessfully(t *testing.T) {
-    router := setupRouter()
+	router := setupRouter()
 
-    w := httptest.NewRecorder()
-    req, _ := http.NewRequest("GET", "/albums/1", nil)
-    router.ServeHTTP(w, req)
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/albums/1", nil)
+	router.ServeHTTP(w, req)
 
-    expectedResponse := `{"id":"1","title":"The Dark Side Of the Moon","artist":"Pink Floyd","price":99.99}`
-    assert.Equal(t, 200, w.Code)
-    assert.JSONEq(t, expectedResponse, w.Body.String())
+	expectedResponse := `{"id":"1","title":"The Dark Side Of the Moon","artist":"Pink Floyd","price":99.99}`
+	assert.Equal(t, 200, w.Code)
+	assert.JSONEq(t, expectedResponse, w.Body.String())
 }
 
-
-
 func TestGetAlbumByIDNotFound(t *testing.T) {
-    router := setupRouter()
+	router := setupRouter()
 
-    w := httptest.NewRecorder()
-    req, _ := http.NewRequest("GET", "/albums/99", nil)
-    router.ServeHTTP(w, req)
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/albums/99", nil)
+	router.ServeHTTP(w, req)
 
-    expectedResponse := `{"message":"album not found"}`
-    assert.Equal(t, 404, w.Code)
-    assert.JSONEq(t, expectedResponse, w.Body.String())
+	expectedResponse := `{"message":"album not found"}`
+	assert.Equal(t, 404, w.Code)
+	assert.JSONEq(t, expectedResponse, w.Body.String())
 }
 
 func TestPostAlbumSuccessfully(t *testing.T) {
-    router := setupRouter()
-    newAlbum := Album {
-        ID: "3",
-        Title: "Holy Diver",
-        Artist: "Ronnie James Dio",
-        Price: 9999.99,
-    }
-    newAlbumJSON, _ := json.Marshal(newAlbum)
+	router := setupRouter()
+	newAlbum := Album{
+		ID:     "3",
+		Title:  "Holy Diver",
+		Artist: "Ronnie James Dio",
+		Price:  9999.99,
+	}
+	newAlbumJSON, _ := json.Marshal(newAlbum)
 
-    w := httptest.NewRecorder()
-    req, _ := http.NewRequest("POST", "/albums", strings.NewReader(string(newAlbumJSON)))
-    router.ServeHTTP(w, req)
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/albums", strings.NewReader(string(newAlbumJSON)))
+	router.ServeHTTP(w, req)
 
-    assert.Equal(t, 201, w.Code)
+	assert.Equal(t, 201, w.Code)
 }
 
 func TestPostAlbumBadRequest(t *testing.T) {
-    router := setupRouter()
-    invalidBodyJSON := `{"aa":"1"}`
+	router := setupRouter()
+	invalidBodyJSON := `{"aa":"1"}`
 
-    w := httptest.NewRecorder()
-    req, _ := http.NewRequest("POST", "/albums", strings.NewReader(invalidBodyJSON))
-    router.ServeHTTP(w, req)
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/albums", strings.NewReader(invalidBodyJSON))
+	router.ServeHTTP(w, req)
 
-    assert.Equal(t, 400, w.Code)
+	assert.Equal(t, 400, w.Code)
 }
-
 
 func TestPostAlbumConflict(t *testing.T) {
-    router := setupRouter()
-    newAlbum := Album {
-        ID: "3",
-        Title: "Holy Diver",
-        Artist: "Ronnie James Dio",
-        Price: 9999.99,
-    }
-    newAlbumJSON, _ := json.Marshal(newAlbum)
+	router := setupRouter()
+	newAlbum := Album{
+		ID:     "3",
+		Title:  "Holy Diver",
+		Artist: "Ronnie James Dio",
+		Price:  9999.99,
+	}
+	newAlbumJSON, _ := json.Marshal(newAlbum)
 
-    w := httptest.NewRecorder()
-    http.NewRequest("POST", "/albums", strings.NewReader(string(newAlbumJSON)))
-    req, _ := http.NewRequest("POST", "/albums", strings.NewReader(string(newAlbumJSON)))
-    router.ServeHTTP(w, req)
+	w := httptest.NewRecorder()
+	http.NewRequest("POST", "/albums", strings.NewReader(string(newAlbumJSON)))
+	req, _ := http.NewRequest("POST", "/albums", strings.NewReader(string(newAlbumJSON)))
+	router.ServeHTTP(w, req)
 
-    assert.Equal(t, 409, w.Code)
+	assert.Equal(t, 409, w.Code)
 }
-
